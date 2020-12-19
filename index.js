44e8fe522022a12d6e72505c54c5b8075f02a134
Ally Kadel <akadel01@gmail.com>
2020-12-19 00:39:32 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,38 @@
+function getName(node){
+  return node["name"];
+}
+
+function headNode(linkedList, collection){
+  return collection[linkedList];
+}
+
+function next(node, collection){
+  return collection[node["next"]];
+}
+
+function nodeAt(index, linkedList, collection){
+  let node = collection[linkedList];
+  for (let i=0; i<index; i++){
+    node = next(node, collection);
+  }
+  return node;
+}
+
+function addressAt(index, linkedList, collection){
+  if (index === 0){
+    return linkedList;
+  }
+  let node = nodeAt(index-1, linkedList, collection);
+  return node["next"];
+}
+
+function indexAt(node, collection, linkedList){
+  let i = 0;
+  let flag = false;
+  while (flag === false){
+    if (nodeAt(i, linkedList, collection) === node){
+      flag = true;
+    }
+  }
+  return i;
+}
